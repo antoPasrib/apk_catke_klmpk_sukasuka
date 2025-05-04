af044bdad8e8143f25ddb581749b9a6ed282af02
Raphael Felio Bagaskara <71231049@students.ukdw.ac.id>
2025-05-04 14:34:53 +0700

    membuatLoginForm

--- src/main/java/com/example/apkpencatatankeuangan/HelloApplication.java
+++ src/main/java/com/example/apkpencatatankeuangan/HelloApplication.java
@@ -2,22 +2,48 @@ package com.example.apkpencatatankeuangan;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage = stage;
+        primaryStage.setTitle("Money Flow 1.0");
+        primaryStage.setScene(new Scene(loadFXML("Login-view")));
+        primaryStage.show();
+    }
+    private static Parent loadFXML(String fxml) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fxml + ".fxml"));
+        try {
+            return fxmlLoader.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
+    public static void setRoot(String fxml, boolean resizable) {
+        primaryStage.getScene().setRoot(loadFXML(fxml));
+        primaryStage.setResizable(resizable);
+        primaryStage.sizeToScene();
+    }
     public static void main(String[] args) {
         launch();
+    }
+    public static void openViewWithModal(String fxml, boolean resizable) {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loadFXML(fxml)));
+        stage.setResizable(resizable);
+        stage.sizeToScene();
+        stage.initOwner(primaryStage);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.showAndWait();
     }
 }
